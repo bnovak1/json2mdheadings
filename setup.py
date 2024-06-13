e68077ae5d4a0862bdad88a3d7f87668e9f9caa8
Brian Novak <bnovak1@users.noreply.github.com>
2024-06-13 14:46:12 -0500

    Change version to 0.0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='json2mdheadings',
-    version='1.0.0',
-    description='A Python package for converting JSON data to Markdown with keys as headings up to the 6th level, then using nested lists after that.',
+    version='0.0.1',
+    description='A Python package for converting JSON data to Markdown with keys as headings.',
     author='Brian Novak',
     author_email='tajrkala@gmail.com',
     url='https://github.com/bnovak1/json2mdheadings',
     packages=find_packages(),
     install_requires=[
-        # Add any dependencies required by your package here
     ]
 )
